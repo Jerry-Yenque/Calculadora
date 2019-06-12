31430b81a3fa1ffc444eb3d1a5efa654a05a541b
Enyioma Osondu <o.enyioma@gmail.com>
2019-06-12 17:33:53 +0100

    changed misleading acc variable name to display and fixed a bug

--- ScientificCalc.py
+++ ScientificCalc.py
@@ -9,9 +9,6 @@ expression = ""
 memory = ""
 inverted = False
 
-# Todo: if F-E is toggled, use exp_format, else use decimal format.
-# Todo: if value is in exp format, change it to decimal.
-
 
 class Format:
     def __init__(self, value: str):
@@ -77,17 +74,6 @@ class Format:
 
         return value
 
-    # ! Deprecated method, made redundant by python's eval()
-    @staticmethod
-    def decimal_format(value):
-        e_ind = value.index("e")
-        if "e+" in value:
-            value = float(value[:e_ind]) * (10 ** int(value[e_ind + 2:]))
-
-        elif "e-" in value:
-            value = float(value[:e_ind]) / (10 ** int(value[e_ind + 2:]))
-        return value
-
 
 def angle_value(value):
     if degreeUnit.get() == 0:
@@ -125,7 +111,7 @@ def clear_expression():
 
 def expression_append(val):
     if new_operand is not True:
-        expressionDisplay.set(expressionDisplay.get() + acc.get() + val)
+        expressionDisplay.set(expressionDisplay.get() + display.get() + val)
     else:
         expressionDisplay.set(expressionDisplay.get() + val)
 
@@ -142,9 +128,9 @@ def get_result():
         elif expression[-2] in ("+", "-"):
             if brackets == 0:
                 return eval(expression + "0")
-        return acc.get()
+        return display.get()
     except ZeroDivisionError:
-        acc.set("Math Error")
+        display.set("Math Error")
 
 
 def press(val):
@@ -152,29 +138,28 @@ def press(val):
     global expression
     global brackets
 
-    if len(acc.get()) < 19 or new_operand is True:
+    if len(display.get()) < 19 or new_operand is True:
         if new_operand is False:
-            if "e" in acc.get() and acc.get()[-1] == "0":
-                acc.set(acc.get()[:-1])
-            if val == 0 and acc.get()[0] == "0" and len(acc.get()) == 1:
-                acc.set("0")
-            elif val == "." and acc.get()[0] == "0":
-                acc.set(acc.get() + val)
-            if expression:
-                if expression[-1] == ")":
-                    expression = expression[:-1] + str(val) + ")"
+            if "e" in display.get() and display.get()[-1] == "0":
+                display.set(display.get()[:-1])
+            if val == 0 and display.get()[0] == "0" and len(display.get()) == 1:
+                display.set("0")
+            elif val == "." and display.get()[0] == "0":
+                display.set(display.get() + val)
+            if expression and expression[-1] == ")":
+                expression = expression[:-1] + str(val) + ")"
             else:
                 if val != ".":
-                    if abs(float(acc.get() + str(val))) >= 1:
-                        acc.set((acc.get() + str(val)).lstrip("0"))
+                    if abs(float(display.get() + str(val))) >= 1:
+                        display.set((display.get() + str(val)).lstrip("0"))
                     else:
-                        acc.set(acc.get() + str(val))
+                        display.set(display.get() + str(val))
 
         else:
-            acc.set(val)
+            display.set(val)
             if expression:
                 if expression[-2] == "/":
-                    expression += acc.get() + ")"
+                    expression += display.get() + ")"
                     brackets -= 1
             new_operand = False
 
@@ -188,38 +173,38 @@ def evaluate():
                 result = round(get_result(), 10)
             else:
                 if brackets == 1:
-                    expression += acc.get() + ")"
+                    expression += display.get() + ")"
                     brackets -= 1
                     result = round(eval(expression), 10)
                 else:
-                    result = round(eval(expression + acc.get()), 10)
-            acc.set(Format(result))
+                    result = round(eval(expression + display.get()), 10)
+            display.set(Format(result))
             clear_expression()
 
         else:
-            if "e" in acc.get():
-                acc.set(Format(str(eval(acc.get()))).result_format())
+            if "e" in display.get():
+                display.set(Format(str(eval(display.get()))).result_format())
             clear_expression()
 
     except ZeroDivisionError:
-        acc.set("Math Error")
+        display.set("Math Error")
 
 
 def clear(case):
     if case == 1:
-        if len(acc.get()) > 1:
-            acc.set(acc.get()[:-1])
+        if len(display.get()) > 1:
+            display.set(display.get()[:-1])
 
-        elif len(acc.get()) == 1:
-            acc.set("0")
+        elif len(display.get()) == 1:
+            display.set("0")
 
     elif case == 2:
-        acc.set("0")
+        display.set("0")
         if len(expressionDisplay.get().split()) == 1:
             clear_expression()
 
     else:
-        acc.set("0")
+        display.set("0")
         clear_expression()
 
 
@@ -266,22 +251,22 @@ def mod_memory(action):
 
     elif action == "recall":
         if memory:
-            acc.set(memory)
+            display.set(memory)
 
     elif action == "save":
-        memory = acc.get()
+        memory = display.get()
 
     elif action == "add":
         if memory:
-            memory = str(eval(memory) + eval(acc.get()))
+            memory = str(eval(memory) + eval(display.get()))
         else:
-            memory = acc.get()
+            memory = display.get()
 
     elif action == "sub":
         if memory:
-            memory = str(eval(memory) - eval(acc.get()))
+            memory = str(eval(memory) - eval(display.get()))
         else:
-            memory = "-" + acc.get()
+            memory = "-" + display.get()
 
 
 def operate(operator):
@@ -291,35 +276,35 @@ def operate(operator):
 
     if expression:
         if expression[-1] == ")":
-            acc.set("")
+            display.set("")
 
     if operator == "+":
-        expression += acc.get() + " + "
+        expression += display.get() + " + "
         expression_append(" + ")
 
     elif operator == "-":
-        expression += acc.get() + " - "
+        expression += display.get() + " - "
         expression_append(" - ")
 
     elif operator == "/":
-        expression += acc.get() + " / "
+        expression += display.get() + " / "
         expression_append(" / ")
 
     elif operator == "*":
-        expression += acc.get() + " * "
+        expression += display.get() + " * "
         expression_append(" * ")
 
     elif operator == "mod":
-        expression += acc.get() + " % "
+        expression += display.get() + " % "
         expression_append(" mod ")
 
     elif operator == "^":
-        expression += acc.get() + " ** "
+        expression += display.get() + " ** "
         expression_append(" ^ ")
 
     elif operator == "root":
         brackets += 1
-        expression += acc.get() + " ** (1 / "
+        expression += display.get() + " ** (1 / "
         expression_append(" yroot ")
 
     elif operator == "(":
@@ -328,148 +313,148 @@ def operate(operator):
         brackets += 1
 
     elif operator == ")":
-        expression += acc.get() + ")"
+        expression += display.get() + ")"
         expression_append(")")
         brackets -= 1
 
     if operator != "root":
-        acc.set(get_result())
+        display.set(get_result())
 
     new_operand = True
 
 
 def exponential():
-    acc.set(acc.get() + "e+0")
+    display.set(display.get() + "e+0")
 
 
 def func(function):
     global new_operand
 
-    value = eval(acc.get())
+    value = eval(display.get())
 
     if function == "neg":
-        acc.set(-value)
+        display.set(-value)
 
     elif function == "sqrt":
         expressionDisplay.set(expressionDisplay.get() + f"sqrt({value}) ")
         new_operand = True
-        acc.set(round(math.sqrt(value), 10))
+        display.set(round(math.sqrt(value), 10))
 
     elif function == "per":
-        acc.set(value / 100)
+        display.set(value / 100)
 
     elif function == "rec":
         expressionDisplay.set(expressionDisplay.get() + f"reciproc({value})")
         new_operand = True
-        acc.set(round((1 / value), 10))
+        display.set(round((1 / value), 10))
 
     elif function == "ln":
         if not inverted:
             try:
                 expressionDisplay.set(expressionDisplay.get() + f"ln({value})")
-                acc.set(round(math.log(value), 10))
+                display.set(round(math.log(value), 10))
             except ValueError:
-                acc.set("Math Error")
+                display.set("Math Error")
         else:
             expressionDisplay.set(expressionDisplay.get() + f"powe({value})")
-            acc.set(round(math.pow(math.e, value), 10))
+            display.set(round(math.pow(math.e, value), 10))
         new_operand = True
 
     elif function == "sinh":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"sinh({value})")
-            acc.set(round(math.sinh(angle_value(value)), 10))
+            display.set(round(math.sinh(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"asinh({value})")
-            acc.set(round(math.asinh(value), 10))
+            display.set(round(math.asinh(value), 10))
         new_operand = True
 
     elif function == "sin":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"sin({value})")
-            acc.set(round(math.sin(angle_value(value)), 10))
+            display.set(round(math.sin(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"asin({value})")
-            acc.set(round(arc_angle_value(math.asin(value)), 10))
+            display.set(round(arc_angle_value(math.asin(value)), 10))
         new_operand = True
 
     elif function == "sqr":
         expressionDisplay.set(expressionDisplay.get() + f"sqr({value})")
         new_operand = True
-        acc.set(round(math.pow(value, 2), 10))
+        display.set(round(math.pow(value, 2), 10))
 
     elif function == "fact":
         try:
             expressionDisplay.set(expressionDisplay.get() + f"fact({value})")
             new_operand = True
-            acc.set(Format(str(math.factorial(value))))
+            display.set(Format(str(math.factorial(value))))
         except ValueError:
-            acc.set("Math Error")
+            display.set("Math Error")
 
     elif function == "cosh":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"cosh({value})")
-            acc.set(round(math.cosh(angle_value(value)), 10))
+            display.set(round(math.cosh(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"acosh({value})")
-            acc.set(round(math.acosh(value), 10))
+            display.set(round(math.acosh(value), 10))
         new_operand = True
 
     elif function == "cos":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"cos({value})")
-            acc.set(round(math.cos(angle_value(value)), 10))
+            display.set(round(math.cos(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"acos({value})")
-            acc.set(round(arc_angle_value(math.acos(value)), 10))
+            display.set(round(arc_angle_value(math.acos(value)), 10))
         new_operand = True
 
     elif function == "pi":
         if not inverted:
-            acc.set(round(math.pi, 10))
+            display.set(round(math.pi, 10))
         else:
-            acc.set(round(math.pi * 2, 10))
+            display.set(round(math.pi * 2, 10))
 
     elif function == "tanh":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"tanh({value})")
-            acc.set(round(math.tanh(angle_value(value)), 10))
+            display.set(round(math.tanh(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"atanh({value})")
-            acc.set(round(math.atanh(value), 10))
+            display.set(round(math.atanh(value), 10))
         new_operand = True
 
     elif function == "tan":
         if not inverted:
             expressionDisplay.set(expressionDisplay.get() + f"tanh({value})")
-            acc.set(round(math.tan(angle_value(value)), 10))
+            display.set(round(math.tan(angle_value(value)), 10))
         else:
             expressionDisplay.set(expressionDisplay.get() + f"atan({value})")
-            acc.set(round(arc_angle_value(math.atan(value)), 10))
+            display.set(round(arc_angle_value(math.atan(value)), 10))
         new_operand = True
 
     elif function == "cube":
         expressionDisplay.set(expressionDisplay.get() + f"cube({value})")
         new_operand = True
-        acc.set(round(math.pow(value, 3), 10))
+        display.set(round(math.pow(value, 3), 10))
 
     elif function == "crt":
         expressionDisplay.set(expressionDisplay.get() + f"crt({value})")
         new_operand = True
-        acc.set(round(math.pow(value, 1 / 3), 10))
+        display.set(round(math.pow(value, 1 / 3), 10))
 
     elif function == "alog":
         expressionDisplay.set(expressionDisplay.get() + f"powerten({value})")
         new_operand = True
-        acc.set(round(math.pow(10, value)))
+        display.set(round(math.pow(10, value)))
 
     elif function == "log":
         try:
             expressionDisplay.set(expressionDisplay.get() + f"log({value})")
             new_operand = True
-            acc.set(round(math.log10(value), 10))
+            display.set(round(math.log10(value), 10))
         except ValueError:
-            acc.set("Math Error")
+            display.set("Math Error")
 
     elif function == "dms":
         if not inverted:
@@ -477,19 +462,19 @@ def func(function):
             minutes, seconds = int((ms * 60) // 1), ((ms * 60) % 1) * 60
             new_val = f"{degree}.{minutes}" + str(seconds).replace(".", "")
             expressionDisplay.set(expressionDisplay.get() + f"dms({value})")
-            acc.set(round(eval(new_val), 10))
+            display.set(round(eval(new_val), 10))
         else:
             degree, ms = int(value // 1), value % 1
             minutes, seconds = ((ms // 0.01) / 60), (((ms % 0.01) * 1000) / 3600)
             new_val = degree + minutes + seconds
             expressionDisplay.set(expressionDisplay.get() + f"degrees({value})")
-            acc.set(round(new_val), 10)
+            display.set(round(new_val), 10)
         new_operand = True
 
     elif function == "int":
         expressionDisplay.set(expressionDisplay.get() + f"Int({value})")
         new_operand = True
-        acc.set(int(value))
+        display.set(int(value))
 
 
 def create_num_button(val, row, col):
@@ -509,9 +494,9 @@ if __name__ == "__main__":
     expressionDisplay = ttk.StringVar()
     displayLabel1 = ttk.Entry(displayFrame, state="disabled", disabledbackground="white", justify="right", font="segoeui 10", highlightthickness=0, borderwidth=0, textvariable=expressionDisplay)
 
-    acc = ttk.StringVar()
-    acc.set("0")
-    displayLabel2 = ttk.Entry(displayFrame, state="disabled", disabledbackground="white", justify="right", font="segoeui 25", highlightthickness=0, borderwidth=0, textvariable=acc)
+    display = ttk.StringVar()
+    display.set("0")
+    displayLabel2 = ttk.Entry(displayFrame, state="disabled", disabledbackground="white", justify="right", font="segoeui 25", highlightthickness=0, borderwidth=0, textvariable=display)
 
     displayLabel1.grid(row=0, sticky="ew")
     displayLabel2.grid(row=1, sticky="ew")
@@ -596,7 +581,7 @@ if __name__ == "__main__":
     buttonTan.grid(row=3, column=2, sticky="ew", padx=2, pady=2)
     buttonCube = ttk.Button(functionFrame, text="x\u00B3", command=lambda: func("cube")).grid(row=3, column=3, sticky="ew", padx=2, pady=2)
     buttonCrt = ttk.Button(functionFrame, text="\u00B3\u221Ax", command=lambda: func("crt")).grid(row=3, column=4, sticky="ew", padx=2, pady=2)
-    buttonFE = ttk.Button(functionFrame, text="F-E", command=lambda: acc.set(Format(acc.get()).exp_format())).grid(row=4, column=0, sticky="ew", padx=2, pady=2)
+    buttonFE = ttk.Button(functionFrame, text="F-E", command=lambda: display.set(Format(display.get()).exp_format())).grid(row=4, column=0, sticky="ew", padx=2, pady=2)
     buttonExp = ttk.Button(functionFrame, text="Exp", command=exponential).grid(row=4, column=1, sticky="ew", padx=2, pady=2)
     buttonMod = ttk.Button(functionFrame, text="Mod", command=lambda: operate("mod")).grid(row=4, column=2, sticky="ew", padx=2, pady=2)
     buttonLog = ttk.Button(functionFrame, text="log", command=lambda: func("log")).grid(row=4, column=3, sticky="ew", padx=2, pady=2)
